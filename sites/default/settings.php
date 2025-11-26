3776a7a3a6cce8d0cdd6415292ae7f9ddaeb11c6
nandinigowda511 <nandinigowda511@gmail.com>
2025-11-26 17:10:14 +0530

    global config and custom theme and new contrib modules added

---
+++ sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'gs26_wl',
+  'username' => 'gs26_wl',
+  'password' => 'password',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'dx1tICCxlH0NtlG6fT7jd6RZSv9fXUhGrpWCYpqyiUvJIkMGGOIaFiGb6VBHVtmWCUh6yxWfQg';
+$settings['config_sync_directory'] = 'sites/default/files/config_N8ssRzF-cm_nyCzeNzzgeGkAzZCEPx3DiyNs6kQiqqRLtsrmZTfcZabKpdmNQuFF-SkOrtRpaw/sync';
